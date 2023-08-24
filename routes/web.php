da6500ce3e60f8655ceaf48ec3bc6f0ee6245a86
BENZERDJEB MOHAMED Adnane <skillzdev@hotmail.com>
2023-08-24 20:32:45 +0100

    fixing typos

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ProductController::class, 'index'])->name('index');
+Route::get('/{product}', [ProductController::class, 'show'])->name('product.show');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
