9e9d86da81d41fae9aca0cc458cab035f77d5f6d
skillz <skillzdev@hotmail.com>
2023-08-25 12:50:07 +0100

    renaming funs

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,16 @@ Route::get('/auth/login', [LoginController::class, 'showLoginForm'])->name('auth
 Route::post('/auth/login', [LoginController::class, 'login']);
 Route::match(['get', 'post'], '/auth/logout', [LoginController::class, 'logout'])->name('auth.logout');
 
-Route::get('/admin/products', [ProductController::class, 'adminIndex'])->name('admin.products.index');
-Route::get('/admin/products/create', [ProductController::class, 'adminCreate'])->name('admin.products.create');
-Route::post('/admin/products', [ProductController::class, 'adminStore'])->name('admin.products.store');
-Route::get('/admin/products/{product}/edit', [ProductController::class, 'adminEdit'])->name('admin.products.edit');
-Route::put('/admin/products/{product}', [ProductController::class, 'adminUpdate'])->name('admin.products.update');
-Route::delete('/admin/products/{product}', [ProductController::class, 'adminDestroy'])->name('admin.products.destroy');
+
+Route::get('/admin/products', [ProductController::class, 'adminProductIndex'])->name('admin.products.index');
+Route::get('/admin/products/create', [ProductController::class, 'adminProductCreate'])->name('admin.products.create');
+Route::post('/admin/products', [ProductController::class, 'adminProductStore'])->name('admin.products.store');
+Route::get('/admin/products/{product}/edit', [ProductController::class, 'adminProductEdit'])->name('admin.products.edit');
+Route::put('/admin/products/{product}', [ProductController::class, 'adminProductUpdate'])->name('admin.products.update');
+Route::delete('/admin/products/{product}', [ProductController::class, 'adminProductDestroy'])->name('admin.products.destroy');
 
 
-Route::get("/admin/orders", [OrderController::class, "adminIndex"])->name("admin.orders.index");
-Route::get("/admin/orders/{order}", [OrderController::class, "adminShow"])->name("admin.orders.show");
-Route::put("/admin/orders/{order}", [OrderController::class, "adminUpdate"])->name("admin.orders.update");
-Route::delete("/admin/orders/{order}", [OrderController::class, "adminDestroy"])->name("admin.orders.destroy");
+Route::get("/admin/orders", [OrderController::class, "adminOrderIndex"])->name("admin.orders.index");
+Route::get("/admin/orders/{order}", [OrderController::class, "adminOrderShow"])->name("admin.orders.show");
+Route::put("/admin/orders/{order}", [OrderController::class, "adminOrderUpdate"])->name("admin.orders.update");
+Route::delete("/admin/orders/{order}", [OrderController::class, "adminOrderDestroy"])->name("admin.orders.destroy");
