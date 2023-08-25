a1ab701634308a494a7a9a0252a298ecc60e4643
BENZERDJEB MOHAMED Adnane <skillzdev@hotmail.com>
2023-08-25 13:46:17 +0100

    added logic to admincontroller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -17,28 +19,50 @@ class AdminController extends Controller
 
     public function adminProductIndex(): View
     {
-        return view('admin.products.index');
+        $products = Product::all();
+        return view('admin.products.index', ['products' => $products]);
     }
     public function adminProductCreate(): View
     {
         return view('admin.products.create');
     }
-    public function adminProductStore()
+    public function adminProductStore(Request $request)
     {
-        //TODO TO IMPLEMENT
+        $validated = $request->validate([
+            'name' => 'required|min:3|max:255',
+            'description' => 'required|min:10|max:1000',
+            'price' => 'required|numeric|min:10|max:100000',
+            'stock' => 'required|numeric|min:0|max:1000',
+            'image' => 'required'
+        ]);
+        $product = Product::create($validated);
+        $product->save();
+        session()->flash('success', 'Product created successfully');
+        return redirect()->route('admin.products.index');
     }
     public function adminProductEdit(): View
     {
         return view('admin.products.edit');
     }
-    public function adminProductUpdate()
+    public function adminProductUpdate(Product $product, Request $request)
     {
-        //TODO TO IMPLEMENT
+        $validated = $request->validate([
+            'name' => 'required|min:3|max:255',
+            'description' => 'required|min:10|max:1000',
+            'price' => 'required|numeric|min:10|max:100000',
+            'stock' => 'required|numeric|min:0|max:1000',
+            'image' => 'required'
+        ]);
+        $product->update($validated);
+        session()->flash('success', 'Product updated successfully');
+        return redirect()->route('admin.products.index');
     }
 
-    public function adminProductDestroy()
+    public function adminProductDestroy(Product $product)
     {
-        //TODO TO IMPLEMENT
+        $product->delete();
+        session()->flash('success', 'Product deleted successfully');
+        return redirect()->route('admin.products.index');
     }
 
 
@@ -52,12 +76,19 @@ class AdminController extends Controller
     {
         return view('admin.orders.show');
     }
-    public function adminOrderUpdate()
+    public function adminOrderUpdate(Order $order, Request $request)
     {
-        //TODO TO IMPLEMENT
+        $validated = $request->validate([
+            'status' => 'required|in:pending,processing,completed,cancelled'
+        ]);
+        $order->update($validated);
+        session()->flash('success', 'Order updated successfully');
+        return redirect()->route('admin.orders.index');
     }
-    public function adminOrderDestroy()
+    public function adminOrderDestroy(Order $order)
     {
-        //TODO TO IMPLEMENT
+        $order->delete();
+        session()->flash('success', 'Order deleted successfully');
+        return redirect()->route('admin.orders.index');
     }
 }
