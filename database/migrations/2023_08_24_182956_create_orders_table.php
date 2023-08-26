eeed45090bd11474bebc3da0080949003e17b4ae
BENZERDJEB MOHAMED Adnane <skillzdev@hotmail.com>
2023-08-26 13:47:20 +0100

    modified schema

--- database/migrations/2023_08_24_182956_create_orders_table.php
+++ database/migrations/2023_08_24_182956_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("product_id")->constrained();
+            $table->foreignId("product_id")->constrained()->cascadeOnDelete();
             $table->string("name");
             $table->string("phone");
             $table->string("city");
