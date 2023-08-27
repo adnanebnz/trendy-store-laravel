5ebb3956994892801535fd3e4d4453554d0179f6
BENZERDJEB MOHAMED Adnane <skillzdev@hotmail.com>
2023-08-27 23:47:42 +0100

    fixing order dashboard

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -13,8 +13,13 @@
                         <tr>
                             <th scope="col"
                                 class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">Nom</th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"></th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"></th>
+                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                Téléphone</th>
+                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Prix
+                                Totale</th>
+                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status
+                            </th>
+                            <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-3"></th>
                             <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-3"></th>
                         </tr>
                     </thead>
@@ -23,6 +28,31 @@
                             <tr class="even:bg-gray-50">
                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
                                     {{ $order->name }}</td>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                                    {{ $order->phone }}</td>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                                    {{ $order->total_price }}</td>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                                    @if ($order->status->value == 'pending')
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                                            En attente
+                                        </span>
+                                    @endif
+                                    @if ($order->status->value == 'processing')
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
+                                            En cours
+                                        </span>
+                                    @endif
+                                    @if ($order->status->value == 'shipped')
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Terminé
+                                        </span>
+                                    @endif
+                                </td>
+
 
                                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                     <a href="{{ route('admin.orders.edit', ['order' => $order]) }}"
