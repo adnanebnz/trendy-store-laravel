6f97235b97dca54c23f5075a956fc7d9893df6ea
skillz <skillzdev@hotmail.com>
2023-08-29 15:36:24 +0100

    modifed product style

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -48,12 +48,12 @@
             <a href="{{ route('product.show', ['product' => $product]) }}"
                 class="flex items-center py-5 px-7 font-semibold bg-slate-700 hover:bg-slate-900 transition text-slate-50 rounded-full">
 
-                المزيد
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6 ml-2">
+                    stroke="currentColor" class="w-6 h-6 mr-2">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
+                شراء الاَن
 
             </a>
         @else
