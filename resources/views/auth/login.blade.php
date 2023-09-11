467fcf3746714d3d3cddd58475e730bba70c508d
BENZERDJEB Adnane <skillzdev@hotmail.com>
2023-09-11 14:09:01 +0100

    fixing UI of login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,16 @@
 <x-auth-layout title="تسجيل الدخول" :action="route('auth.login')" submitMessage="تسجيل الدخول">
-    <x-input name="email" label="Adresse e-mail" type="email" />
-    <x-input name="password" label="Mot de passe" type="password" />
+    <x-input name="email" label="البريد الإلكتروني" type="email" />
+    <x-input name="password" label="كلمة السر" type="password" />
     <div class="flex items-center justify-between">
-        <div class="flex items-center">
+        <div>
+            {{-- <a href="#"
+                class="underline underline-offset-4 text-amber-600 hover:text-amber-500 hover:cursor-pointer">
+                نسيت كلمة السر؟</a> --}}
+        </div>
+        <div class="flex items-center justify-end">
             <input id="remember" name="remember" type="checkbox"
                 class="form-checkbox h-4 w-4 rounded border-gray-300 text-amber-600 focus:ring-amber-600">
-            <label for="remember" class="ml-3 block text-sm leading-6 text-gray-900">Rester connecté</label>
+            <label for="remember" class="ml-3 block text-sm leading-6 text-gray-900">أبقني متصلاً</label>
         </div>
     </div>
 </x-auth-layout>
